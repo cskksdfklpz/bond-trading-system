c6f949a460f633c11ae3060752e2f748ba68a985
Quanzhi Bi <6913631-quanzhibi@users.noreply.gitlab.com>
2020-12-22 04:20:33 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
     BondInfo::init();
 
     /* trades.txt 
-	 *     	|
+     *     	|
 	 *     	v		  (port=1236)	
 	 * (data_reader ->  TCP/IP -> BondTradeBookingConnector) 
 	 * 		|
